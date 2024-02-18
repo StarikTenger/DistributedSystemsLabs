13106f0b7defb2ff59d752d9826a951017ccdcd1
StarikTenger <StarikTenger@gmail.com>
2024-02-18 18:32:22 +0100

    ы2

--- tp2/chat/ServerImpl.java
+++ tp2/chat/ServerImpl.java
@@ -1,17 +1,24 @@
 package chat;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.*;
 
 public class ServerImpl  implements Server_itf {
 
-        public void connect(Integer id, Client_itf itf) {
+		private Set<Integer> knownIds = new HashSet<>();
+		private HashMap<Integer, Client_itf> connected = new HashMap<>();
 
+        public void connect(Integer id, Client_itf itf) {
+			if (knownIds.contains(id)) {
+				System.out.println("Connected user with ID" + String.valueOf(id));
+				connected.put(id, itf);
+			} else {
+				System.out.println("ERROR: Non-registered ID" + String.valueOf(id));
+			}
         }
 
         @Override
         public void disconnect(Integer id) {
-
+			
         }
 
         @Override
--- tp2/chat/Server_itf.java
+++ tp2/chat/Server_itf.java
@@ -4,6 +4,6 @@ import java.util.*;
 
 public interface Server_itf extends Client_itf {
 	public int genId(); // New id for new user
-	public ArrayList<Client> getClientList();
+	public HashMap<Integer, Client_itf> getClientList();
 	public LinkedList getChatHistory();
 }
