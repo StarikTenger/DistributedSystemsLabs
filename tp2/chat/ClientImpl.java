c619662c66ad9339637f2a61d86615c9db10715b
StarikTenger <StarikTenger@gmail.com>
2024-02-18 22:56:25 +0100

    client connect funcs

--- tp2/chat/ClientImpl.java
+++ tp2/chat/ClientImpl.java
@@ -1,19 +1,22 @@
 package chat;
 
 import java.rmi.RemoteException;
+import java.util.HashMap;
 
 public class ClientImpl implements Client_itf {
     String id;
     ChatHistory chatHistory;
     ClientList clientList;
+	private HashMap<Integer, Client_itf> connected = new HashMap<>();
 
 	@Override
     public void connect(Integer id, Client_itf itf) throws RemoteException {
+		connected.put(id, itf);
     }
 
     @Override
     public void disconnect(Integer id) throws RemoteException {
-
+		connected.remove(id);
     }
 
     @Override
