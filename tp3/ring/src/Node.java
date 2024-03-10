06f19545dc6aa4e6d2d84b861ac1bcce176ff260
StarikTenger <StarikTenger@gmail.com>
2024-03-10 16:38:24 +0100

    почти работает

--- tp3/ring/src/Node.java
+++ tp3/ring/src/Node.java
@@ -1,22 +1,38 @@
 import com.rabbitmq.client.*;
 
-import static java.lang.Math.random;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Date;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 public class Node {
     private String queueName;
-	private int id;
+    private int id;
+    private Status status;
 
-	private void log(String str) {
-		String time = new SimpleDateFormat("HH:mm:ss").format(new Date());
-		System.out.println("[Node " + String.valueOf(id) + " at " + time + "] " + str);
-	}
+    enum Status {
+        IDLE,
+        STARTED,
+        LEADER,
+        NOT_LEADER,
+        ELECTION_IN_PROGRESS
+    }
+
+    private class ElectionMessage {
+        public int senderId;
+        public int maxId;
+    }
+
+    private void log(String str) {
+        String time = new SimpleDateFormat("HH:mm:ss").format(new Date());
+        System.out.println("[Node " + String.valueOf(id) + " at " + time + "] " + str);
+    }
 
     public Node(String queueName) {
-		id = (int) (random()*1000);
+        id = (int) (Math.random() * 1000);
         this.queueName = queueName;
-		log("Created");
+        status = Status.IDLE;
+        log("Created");
     }
 
     public void connect(String nextNodeQueue) throws Exception {
@@ -41,6 +57,11 @@ public class Node {
                     forwardMessage(channel, nextNodeQueue, message);
                 }
 
+                // Check if an election needs to be started
+                if (status == Status.IDLE) {
+                    startElection(channel, nextNodeQueue);
+                }
+
                 //Thread.sleep(1000); // Add some delay between iterations
             }
         }
@@ -50,5 +71,42 @@ public class Node {
         // Forward the message to the next node in the ring
         channel.basicPublish("", nextNodeQueue, null, message.getBytes("UTF-8"));
         System.out.println(queueName + " Forwarded: " + message);
+    }
+
+    private void startElection(Channel channel, String nextNodeQueue) throws Exception {
+        status = Status.ELECTION_IN_PROGRESS;
+        ElectionMessage electionMessage = new ElectionMessage();
+        electionMessage.senderId = id;
+        electionMessage.maxId = id;
+
+        // Send election message to the next node in the ring
+        channel.basicPublish("", nextNodeQueue, null, serializeElectionMessage(electionMessage));
+
+        log("Started Election");
+
+        // Wait for a response from the next node
+        TimeUnit.SECONDS.sleep(2); // Adjust the waiting time based on your requirements
+
+        // Check if this node won the election
+        if (status == Status.ELECTION_IN_PROGRESS) {
+            status = Status.LEADER;
+            log("Elected as Leader");
+        }
+    }
+
+    private byte[] serializeElectionMessage(ElectionMessage message) throws Exception {
+        // Convert ElectionMessage to byte array (You may use serialization libraries like Gson, Jackson, etc.)
+        // For simplicity, we use a simple string format
+        return (message.senderId + "," + message.maxId).getBytes("UTF-8");
+    }
+
+    private ElectionMessage deserializeElectionMessage(byte[] data) throws Exception {
+        // Convert byte array to ElectionMessage (You may use serialization libraries like Gson, Jackson, etc.)
+        // For simplicity, we use a simple string format
+        String[] parts = new String(data, "UTF-8").split(",");
+        ElectionMessage message = new ElectionMessage();
+        message.senderId = Integer.parseInt(parts[0]);
+        message.maxId = Integer.parseInt(parts[1]);
+        return message;
     }
 }
