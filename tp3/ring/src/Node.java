a280238aa8328dd65429cfa21404ad5735b522f2
Olesya Schukina <olesyaschu@gmail.com>
2024-03-10 21:21:02 +0100

    Fixed node

--- tp3/ring/src/Node.java
+++ tp3/ring/src/Node.java
@@ -1,7 +1,9 @@
 import com.rabbitmq.client.*;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeoutException;
 
 public class Node {
     private String queueName;
@@ -28,77 +30,53 @@ public class Node {
         System.out.println("[Node " + String.valueOf(id) + " at " + time + "] " + str);
     }
 
-    public Node(String queueName) {
+    public Node(String qn, String nqn) {
         id = (int) (Math.random() * 1000);
-        this.queueName = queueName;
+        queueName = qn;
         status = Status.IDLE;
+        nextNodeQueue = nqn;
         log("Created");
     }
 
-    public void connect(String nextNodeQueue) {
+    public void connect() throws IOException, TimeoutException {
 		ConnectionFactory factory = new ConnectionFactory();
 		factory.setHost("localhost");
-		this.nextNodeQueue = nextNodeQueue;
 
-		try (Connection connection = factory.newConnection();
-			Channel channel = connection.createChannel()) {
+        Connection connection = factory.newConnection();
+        channel = connection.createChannel();
 
-			this.channel = channel;
-			
-			channel.queueDeclare(queueName, false, false, false, null);
+        channel.queueDeclare(queueName, false, false, false, null);
+        channel.queueDeclare(nextNodeQueue, false, false, false, null);
 
-			DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-				try {
-					String message = new String(delivery.getBody(), "UTF-8");
-					log(queueName + " Received: " + message);
-					handleMessage(message, channel, nextNodeQueue);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			};
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            try {
+                String message = new String(delivery.getBody(), "UTF-8");
+                log(queueName + " Received: " + message);
+                handleMessage(message);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
 
-			channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
-			});
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+        });
 
-			log("Connected " + queueName + " to " + nextNodeQueue);
-
-			//startElection(channel, nextNodeQueue);
-
-			// while (true) {
-			// 	// Check if an election needs to be started
-			// 	if (status == Status.IDLE) {
-			// 		try {
-			// 			startElection(channel, nextNodeQueue);
-			// 		} catch (Exception e) {
-			// 			e.printStackTrace();
-			// 		}
-			// 	}
-
-			// 	// Add some delay between iterations
-			// 	try {
-			// 		Thread.sleep(1000);
-			// 	} catch (InterruptedException e) {
-			// 		e.printStackTrace();
-			// 	}
-			// }
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        log("Connected " + queueName + " to " + nextNodeQueue);
 	}
 
 
-    private void handleMessage(String message, Channel channel, String nextNodeQueue) throws Exception {
+    private void handleMessage(String message) throws Exception {
         if (message.startsWith("ELECTION")) {
             // Handle election message
             ElectionMessage electionMessage = deserializeElectionMessage(message.substring(9));
-            handleElectionMessage(electionMessage, channel, nextNodeQueue);
+            handleElectionMessage(electionMessage);
         } else {
             // Process regular message
             // For simplicity, let's assume processing a regular message involves printing it
             log("Processing Regular Message: " + message);
 
             // Forward the message to the next node in the ring
-            forwardMessage(channel, nextNodeQueue, message);
+            forwardMessage(message);
         }
     }
 
@@ -108,13 +86,14 @@ public class Node {
         electionMessage.senderId = id;
         electionMessage.maxId = id;
 
+        log("nextNodeQueue: " + nextNodeQueue);
         // Send election message to the next node in the ring
         channel.basicPublish("", nextNodeQueue, null, serializeElectionMessage(electionMessage).getBytes("UTF-8"));
 
         log("Started Election");
     }
 
-    private void handleElectionMessage(ElectionMessage electionMessage, Channel channel, String nextNodeQueue) throws Exception {
+    private void handleElectionMessage(ElectionMessage electionMessage) throws Exception {
         log("Received Election Message from Node " + electionMessage.senderId +
                 " with maxId " + electionMessage.maxId);
 
@@ -122,10 +101,10 @@ public class Node {
         if (electionMessage.maxId > id) {
             // Update maxId and forward the election message
             electionMessage.maxId = id;
-            forwardElectionMessage(channel, nextNodeQueue, electionMessage);
+            forwardElectionMessage(electionMessage);
         } else if (electionMessage.senderId != id) {
             // Forward the election message to the next node
-            forwardElectionMessage(channel, nextNodeQueue, electionMessage);
+            forwardElectionMessage(electionMessage);
         } else if (electionMessage.senderId == id && electionMessage.maxId == id) {
             // Node becomes the leader
             status = Status.LEADER;
@@ -133,14 +112,14 @@ public class Node {
         }
     }
 
-    private void forwardElectionMessage(Channel channel, String nextNodeQueue, ElectionMessage electionMessage) throws Exception {
+    private void forwardElectionMessage(ElectionMessage electionMessage) throws Exception {
         // Forward the election message to the next node in the ring
         String serializedMessage = "ELECTION" + serializeElectionMessage(electionMessage);
         channel.basicPublish("", nextNodeQueue, null, serializedMessage.getBytes("UTF-8"));
         log("Forwarded Election Message to Node " + nextNodeQueue);
     }
 
-    private void forwardMessage(Channel channel, String nextNodeQueue, String message) throws Exception {
+    private void forwardMessage(String message) throws Exception {
         // Forward the message to the next node in the ring
         channel.basicPublish("", nextNodeQueue, null, message.getBytes("UTF-8"));
         log(queueName + " Forwarded: " + message);
--- tp3/ring/src/RingTopology.java
+++ tp3/ring/src/RingTopology.java
@@ -1,16 +1,16 @@
 public class RingTopology {
     public static void main(String[] args) {
         // Start three nodes in the ring
-        Node node1 = new Node("node1-queue");
-        Node node2 = new Node("node2-queue");
-        Node node3 = new Node("node3-queue");
+        Node node1 = new Node("node1-queue", "node2-queue");
+        Node node2 = new Node("node2-queue", "node3-queue");
+        Node node3 = new Node("node3-queue", "node1-queue");
 
 
         // Connect nodes in the ring
         try {
-            node1.connect("node2-queue");
-            node2.connect("node3-queue");
-            node3.connect("node1-queue");
+            node1.connect();
+            node2.connect();
+            node3.connect();
 			Thread.sleep(1000);
         } catch (Exception e) {
             e.printStackTrace();
