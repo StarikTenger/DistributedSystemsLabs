ea8ed9a282ebcfc8b233b7e8512444f4e3008e29
Olesya Schukina <olesyaschu@gmail.com>
2024-03-24 19:35:33 +0100

    Added connection

--- project/life/src/Main.java
+++ project/life/src/Main.java
@@ -1,5 +1,12 @@
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.concurrent.TimeoutException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, TimeoutException {
         System.out.println("Hello world!");
+
+        Board board0 = new Board(0, new Integer[] {null, null, null, null, 1, null, null, null});
+        Board board1 = new Board(1, new Integer[] {null, null, null, 0, null, null, null, null});
     }
 }
