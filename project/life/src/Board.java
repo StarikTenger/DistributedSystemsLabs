ab4a135fea28e3649d7e9fbe820f9113e97d48fb
StarikTenger <StarikTenger@gmail.com>
2024-04-08 17:04:33 +0200

    print all table

--- project/life/src/Board.java
+++ project/life/src/Board.java
@@ -204,10 +204,18 @@ public class Board {
 	}
 
 	public void print() {
-		for (int i = 0; i < BOARD_SIZE; i++) {
-			for (int j = 0; j < BOARD_SIZE; j++) {
+		// for (int i = 0; i < BOARD_SIZE; i++) {
+		// 	for (int j = 0; j < BOARD_SIZE; j++) {
+		// 		System.out.print(
+		// 			cells[i + BOARD_SIZE][j + BOARD_SIZE].isAlive ? "X" : ".");
+		// 	}
+		// 	System.out.println();
+		// }
+
+		for (int i = 0; i < BOARD_SIZE * 3; i++) {
+			for (int j = 0; j < BOARD_SIZE * 3; j++) {
 				System.out.print(
-					cells[i + BOARD_SIZE][j + BOARD_SIZE].isAlive ? "X" : ".");
+					cells[i][j].isAlive ? "X" : ".");
 			}
 			System.out.println();
 		}
@@ -269,6 +277,7 @@ public class Board {
     private void handleNeighborTable(int index, CellState[][] neighborCells) {
 		// Coordinates of topleft corner on united board
 		Vec2i delta = getVectorForDirection(Directions.values()[index]).add(new Vec2i(1,1)).mult(BOARD_SIZE);
+		System.out.println(delta);
 
 		for (int i = 0; i < BOARD_SIZE; i++) {
 			for (int j = 0; j < BOARD_SIZE; j++) {
@@ -300,8 +309,9 @@ public class Board {
     }
 
     private CellState getCellState(int x, int y) {
-        if (x < BOARD_SIZE && neighbors[Directions.L.ordinal()] == null) 
+        if (x < BOARD_SIZE && neighbors[Directions.L.ordinal()] == null) {
 			return new CellState();
+		}
 		if (x >= BOARD_SIZE * 2 && neighbors[Directions.R.ordinal()] == null) 
 			return new CellState();
 		if (y < BOARD_SIZE && neighbors[Directions.U.ordinal()] == null) 
